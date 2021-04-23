72e1915166b94ff275360d859366694afb77a0ef
Brian L. McMichael <brian@brianmcmichael.com>
2021-04-23 12:52:22 -0400

    Updates to specification (#14)

--- src/DssVest.sol
+++ src/DssVest.sol
@@ -23,19 +23,20 @@ interface IMKR {
 contract DssVest {
 
     // MKR Mainnet: 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
-    IMKR public immutable MKR;
+    IMKR     public immutable MKR;
+    uint256  public constant  MAX_VEST_PERIOD = 20 * 365 days;
 
     uint256 internal constant WAD = 10**18;
 
     event Rely(address indexed usr);
     event Deny(address indexed usr);
-    event Init(uint256 indexed id);
+    event Init(uint256 indexed id, address indexed usr);
     event Vest(uint256 indexed id);
-    event Move(uint256 indexed id);
+    event Move(uint256 indexed id, address indexed dst);
     event Yank(uint256 indexed id);
 
     // --- Auth ---
-    mapping (address => uint) public wards;
+    mapping (address => uint256) public wards;
     function rely(address usr) external auth { wards[usr] = 1; emit Rely(usr); }
     function deny(address usr) external auth { wards[usr] = 0; emit Deny(usr); }
     modifier auth {
@@ -50,6 +51,7 @@ contract DssVest {
         uint48  fin;   // End of vesting period
         uint128 amt;   // Total reward amount
         uint128 rxd;   // Amount of vest claimed
+        address mgr;   // A manager address that can yank
     }
     mapping (uint256 => Award) public awards;
     uint256 public ids;
@@ -65,15 +67,26 @@ contract DssVest {
         require((z = x - y) <= x);
     }
 
-    function init(address _usr, uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf, uint256 _pmt) external auth returns (uint256 id) {
-        require(_usr != address(0),                     "dss-vest/invalid-user");
-        require(_amt < uint128(-1),                     "dss-vest/amount-error");
-        require(_bgn < block.timestamp + 5 * 365 days,  "dss-vest/bgn-too-far");
-        require(_tau > 0,                               "dss-vest/tau-zero");
-        require(_tau < 5 * 365 days,                    "dss-vest/tau-too-long");
-        require(_clf <= _tau,                           "dss-vest/clf-too-long");
-        require(_pmt < uint128(-1),                     "dss-vest/payout-error");
-        require(_pmt <= _amt,                           "dss-vest/bulk-payment-higher-than-amt");
+    /*
+        @dev Govanance adds a vesting contract
+        @param _usr The recipient of the reward
+        @param _amt The total amount of the vest
+        @param _bgn The starting timestamp of the vest
+        @param _tau The duration of the vest (in seconds)
+        @param _clf The cliff duration in seconds (i.e. 1 years)
+        @param _pmt An optional lump sump payout from the vest amount
+        @param _mgr An optional manager for the contract. Can yank if vesting ends prematurely.
+        @return id  The id of the vesting contract
+    */
+    function init(address _usr, uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf, uint256 _pmt, address _mgr) external auth returns (uint256 id) {
+        require(_usr != address(0),                       "dss-vest/invalid-user");
+        require(_amt < uint128(-1),                       "dss-vest/amount-error");
+        require(_bgn < block.timestamp + MAX_VEST_PERIOD, "dss-vest/bgn-too-far");
+        require(_tau > 0,                                 "dss-vest/tau-zero");
+        require(_tau <= MAX_VEST_PERIOD,                  "dss-vest/tau-too-long");
+        require(_clf <= _tau,                             "dss-vest/clf-too-long");
+        require(_pmt < uint128(-1),                       "dss-vest/payout-error");
+        require(_pmt <= _amt,                             "dss-vest/bulk-payment-higher-than-amt");
 
         id = ++ids;
         if (_amt - _pmt != 0) {      // safe because pmt <= amt
@@ -83,15 +96,20 @@ contract DssVest {
                 clf: uint48(_bgn + _clf),
                 fin: uint48(_bgn + _tau),
                 amt: uint128(_amt - _pmt),
-                rxd: 0
+                rxd: 0,
+                mgr: _mgr
             });
         }
         if (_pmt != 0) {
             MKR.mint(_usr, _pmt);    // Initial payout
         }
-        emit Init(id);
+        emit Init(id, _usr);
     }
 
+    /*
+        @dev Owner of a vesting contract calls this to claim rewards
+        @param id The id of the vesting contract
+    */
     function vest(uint256 _id) external {
         Award memory _award = awards[_id];
         require(_award.usr == msg.sender, "dss-vest/only-user-can-claim");
@@ -108,19 +126,33 @@ contract DssVest {
         emit Vest(_id);
     }
 
-    function yank(uint256 _id) external auth {
+    /*
+        @dev Allows governance or the manager to remove a vesting contract
+        @param id The id of the vesting contract
+    */
+    function yank(uint256 _id) external {
+        require(wards[msg.sender] == 1 || awards[_id].mgr == msg.sender, "dss-vest/not-authorized");
         delete awards[_id];
         emit Yank(_id);
     }
 
-    function move(uint256 _id, address _usr) external {
+    /*
+        @dev Allows owner to move a contract to a different address
+        @param _id  The id of the vesting contract
+        @param _dst The address to send ownership of the contract to
+    */
+    function move(uint256 _id, address _dst) external {
         require(awards[_id].usr == msg.sender, "dss-vest/only-user-can-move");
-        require(_usr != address(0), "dss-vest/zero-address-invalid");
-        awards[_id].usr = _usr;
-        emit Move(_id);
+        require(_dst != address(0), "dss-vest/zero-address-invalid");
+        awards[_id].usr = _dst;
+        emit Move(_id, _dst);
     }
 
-    function live(uint256 _id) external view returns (bool) {
+    /*
+        @dev Return true if a contract is valid
+        @param _id The id of the vesting contract
+    */
+    function valid(uint256 _id) external view returns (bool) {
         return awards[_id].usr != address(0);
     }
 }
