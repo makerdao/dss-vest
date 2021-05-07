0bc19c091ef5de58b36917f074401b67dc11e6a8
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-05-07 23:07:45 +0100

    Remove _pmt seed + fix _bgn seed range + cleanup

--- src/fuzz/DssVestEchidnaTest.sol
+++ src/fuzz/DssVestEchidnaTest.sol
@@ -28,41 +28,34 @@ contract DssVestEchidnaTest is EchidnaInterface {
         require((z = x - y) <= x);
     }
 
-    function test_init(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf, uint256 _pmt) public {
+    function test_init(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf) public {
         _amt = _amt % uint128(-1);
         if (_amt < WAD) _amt = (1 + _amt) * WAD;
-        _bgn = block.timestamp + _bgn % vest.MAX_VEST_PERIOD();
+        _bgn = block.timestamp + _bgn % (block.timestamp + vest.MAX_VEST_PERIOD());
         _tau = 1 + _tau % vest.MAX_VEST_PERIOD();
         _clf = _clf % _tau;
-        _pmt = _pmt % _amt;
-        uint256 id;
-        {
         uint256 prevId = vest.ids();
-        id = vest.init(address(this), _amt, _bgn, _tau, _clf, _pmt, echidna_mgr);
+        uint256 id = vest.init(address(this), _amt, _bgn, _tau, _clf, echidna_mgr);
         assert(vest.ids() == add(prevId, 1));
         assert(vest.ids() == id);
         assert(vest.valid(id));
-        }
         (address usr, uint48 bgn, uint48 clf, uint48 fin, uint128 amt, uint128 rxd, address mgr) = vest.awards(id);
-        if (sub(_amt, _pmt) != 0) {
-            assert(usr == (address(this)));
-            assert(bgn == uint48(_bgn));
-            assert(clf == add(_bgn, _clf));
-            assert(fin == add(_bgn, _tau));
-            assert(amt == sub(_amt, _pmt));
-            assert(rxd == uint128(0));
-            assert(mgr == echidna_mgr);
-        }
+        assert(usr == address(this));
+        assert(bgn == uint48(_bgn));
+        assert(clf == uint48(_bgn + _clf));
+        assert(fin == uint48(_bgn + _tau));
+        assert(amt == uint128(_amt));
+        assert(rxd == 0);
+        assert(mgr == echidna_mgr);
     }
 
-    function test_vest(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf, uint256 _pmt) public {
+    function test_vest(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf) public {
         _amt = _amt % uint128(-1);
         if (_amt < WAD) _amt = (1 + _amt) * WAD;
         _bgn = block.timestamp + _bgn % vest.MAX_VEST_PERIOD();
         _tau = 1 + _tau % vest.MAX_VEST_PERIOD();
         _clf = _clf % _tau;
-        _pmt = _pmt % _amt;
-        uint256 id = vest.init(address(this), _amt, _bgn, _tau, _clf, _pmt, echidna_mgr);
+        uint256 id = vest.init(address(this), _amt, _bgn, _tau, _clf, echidna_mgr);
         assert(vest.valid(id));
         uint256 ids = vest.ids();
         vest.vest(id);
