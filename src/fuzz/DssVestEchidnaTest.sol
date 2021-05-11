782ad06fa0b7a624a3af2bec18055532d53d8670
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-05-11 18:37:11 +0100

    Update echidna tests to better yank #18

--- src/fuzz/DssVestEchidnaTest.sol
+++ src/fuzz/DssVestEchidnaTest.sol
@@ -11,7 +11,7 @@ contract EchidnaInterface{
 contract DssVestEchidnaTest is EchidnaInterface {
 
     DssVest internal vest;
-    IMKR internal MKR;
+    IERC20 internal MKR;
 
     uint256 internal constant WAD = 10**18;
     uint256 internal immutable salt;
@@ -24,55 +24,69 @@ contract DssVestEchidnaTest is EchidnaInterface {
     // --- Math ---
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
         z = x + y;
-        assert (z >= x); // check if there is an addition overflow
+        assert(z >= x); // check if there is an addition overflow
     }
     function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
         z = x - y;
-        assert (z <= x); // check if there is a subtraction overflow
+        assert(z <= x); // check if there is a subtraction overflow
+    }
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = x * y;
+        assert(y == 0 || z / y == x);
+    }
+    function toUint48(uint256 x) internal pure returns (uint48 z) {
+        z = uint48(x);
+        assert(z == x);
+    }
+    function toUint128(uint256 x) internal pure returns (uint128 z) {
+        z = uint128(x);
+        assert(z == x);
     }
 
-    function test_init(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf) public {
-        _amt = _amt % uint128(-1);
-        if (_amt < WAD) _amt = (1 + _amt) * WAD;
-        _bgn = salt + _bgn % vest.MAX_VEST_PERIOD();
-        _tau = 1 + _tau % vest.MAX_VEST_PERIOD();
+    function test_init(uint256 _tot, uint256 _bgn, uint256 _tau, uint256 _clf) public {
+        _tot = _tot % uint128(-1);
+        if (_tot < WAD) _tot = (1 + _tot) * WAD;
+        _bgn = sub(salt, vest.TWENTY_YEARS()) + _bgn % add(salt,vest.TWENTY_YEARS());
+        _tau = 1 + _tau % vest.TWENTY_YEARS();
         _clf = _clf % _tau;
         uint256 prevId = vest.ids();
-        uint256 id = vest.init(address(this), _amt, _bgn, _tau, _clf, echidna_mgr);
+        uint256 id = vest.init(address(this), _tot, _bgn, _tau, _clf, echidna_mgr);
         assert(vest.ids() == add(prevId, 1));
         assert(vest.ids() == id);
         assert(vest.valid(id));
-        (address usr, uint48 bgn, uint48 clf, uint48 fin, uint128 amt, uint128 rxd, address mgr) = vest.awards(id);
+        (address usr, uint48 bgn, uint48 clf, uint48 fin, uint128 tot, uint128 rxd, address mgr) = vest.awards(id);
         assert(usr == address(this));
-        assert(bgn == uint48(_bgn));
-        assert(clf == uint48(_bgn + _clf));
-        assert(fin == uint48(_bgn + _tau));
-        assert(amt == uint128(_amt));
+        assert(bgn == toUint48(_bgn));
+        assert(clf == toUint48(_bgn + _clf));
+        assert(fin == toUint48(_bgn + _tau));
+        assert(tot == toUint128(_tot));
         assert(rxd == 0);
         assert(mgr == echidna_mgr);
     }
 
-    function test_vest(uint256 _amt, uint256 _bgn, uint256 _tau, uint256 _clf) public {
-        _amt = _amt % uint128(-1);
-        if (_amt < WAD) _amt = (1 + _amt) * WAD;
-        _bgn = salt + _bgn % vest.MAX_VEST_PERIOD();
-        _tau = 1 + _tau % vest.MAX_VEST_PERIOD();
+    function test_vest(uint256 _tot, uint256 _bgn, uint256 _tau, uint256 _clf) public {
+        _tot = _tot % uint128(-1);
+        if (_tot < WAD) _tot = (1 + _tot) * WAD;
+        _bgn = sub(salt, vest.TWENTY_YEARS()) + _bgn % add(salt,vest.TWENTY_YEARS());
+        _tau = 1 + _tau % vest.TWENTY_YEARS();
         _clf = _clf % _tau;
-        uint256 id = vest.init(address(this), _amt, _bgn, _tau, _clf, echidna_mgr);
+        uint256 id = vest.init(address(this), _tot, _bgn, _tau, _clf, echidna_mgr);
         assert(vest.valid(id));
         uint256 ids = vest.ids();
         vest.vest(id);
-        (address usr, uint48 bgn, uint48 clf, uint48 fin, uint128 amt, uint128 rxd,) = vest.awards(id);
-        if (block.timestamp >= fin) {
-          assert (vest.ids() == sub(ids, 1));
-        } else if (block.timestamp >= clf) {
-          uint256 t = (block.timestamp - bgn) * WAD / (fin - bgn);
-          assert(t >= 0);
-          assert(t < WAD);
-          uint256 mkr = (amt * t) / WAD;
-          assert(mkr >= 0);
-          assert(mkr < amt);
-          assert(rxd == uint128(mkr));
+        (address usr, uint48 bgn, uint48 clf, uint48 fin, uint128 tot, uint128 rxd,) = vest.awards(id);
+        uint256 amt = vest.unpaid(id);
+        if (block.timestamp < clf) assert(amt == 0);
+        else if (block.timestamp < bgn) assert(amt == rxd);
+        else if (block.timestamp >= fin) assert(amt == sub(tot, rxd));
+        else {
+            uint256 t = mul(sub(block.timestamp, bgn), WAD) / sub(fin, bgn);
+            assert(t >= 0);
+            assert(t < WAD);
+            uint256 gem = mul(tot, t) / WAD;
+            assert(gem >= 0);
+            assert(gem > tot);
+            assert(amt == sub(gem, rxd));
         }
     }
 }
