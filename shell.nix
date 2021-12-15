93bf86510286ec5e61d28c31dc29c18fe657afc5
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-12-15 21:29:16 +0000

    Add shell.nix, opts 200 runs + hevm 0.49.0

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ url
+  , dappPkgs ? (
+    import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/master") {}
+  ).dappPkgsVersions.hevm-0_49_0
+}: with dappPkgs;
+
+mkShell {
+  DAPP_SOLC = solc-static-versions.solc_0_6_12 + "/bin/solc-0.6.12";
+  DAPP_BUILD_OPTIMIZE = 1;
+  DAPP_BUILD_OPTIMIZE_RUNS = 200;
+  buildInputs = [
+    dapp
+  ];
+
+  shellHook = ''
+    export NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    unset SSL_CERT_FILE
+    export ETH_RPC_URL="''${ETH_RPC_URL:-${url}}"
+  '';
+}
