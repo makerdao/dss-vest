acfdaa5efe44d8669bcfb7024797d5c7cc65c47c
Nazzareno Massari <nas@naszam.co>
2022-02-02 16:37:54 +0000

    improve `echidna.sh`

--- echidna.sh
+++ echidna.sh
@@ -3,6 +3,20 @@ set -e
 
 [[ "$1" ]] || { echo "Please specify the vest contract to fuzz (e.g. mintable, suckable, transferrable)"; exit 1; }
 
+case "$1" in
+            mintable)
+                VEST=DssVestMintableEchidnaTest
+                ;;
+            suckable)
+                VEST=DssVestSuckableEchidnaTest
+                ;;
+    transferrable)
+                VEST=DssVestTransferrableEchidnaTest
+                ;;
+    *)          echo "Incorrect vest contract: '$1' (mintable, suckable, transferrable)"; exit 1
+                ;;
+esac
+
 SOLC=~/.solc-select/artifacts/solc-0.6.12
 
 # Check if solc-select is installed
@@ -11,16 +25,4 @@ SOLC=~/.solc-select/artifacts/solc-0.6.12
 # Check if solc-0.6.12 is installed
 [[ -f "$SOLC" ]] || solc-select install 0.6.12
 
-# Run Echidna
-case "$1" in
-            mintable)
-                echidna-test echidna/DssVestMintableEchidnaTest.sol --contract DssVestMintableEchidnaTest --crytic-args "--solc $SOLC" --config echidna.config.yml
-                ;;
-            suckable)
-                echidna-test echidna/DssVestSuckableEchidnaTest.sol --contract DssVestSuckableEchidnaTest --crytic-args "--solc $SOLC" --config echidna.config.yml
-                ;;
-    transferrable)
-                echidna-test echidna/DssVestTransferrableEchidnaTest.sol --contract DssVestTransferrableEchidnaTest --crytic-args "--solc $SOLC" --config echidna.config.yml
-                ;;
-    *)
-esac
+echidna-test echidna/"$VEST".sol --contract "$VEST" --crytic-args "--solc $SOLC" --config echidna.config.yml
