9f814aee8ff58f5886091cfa071dc1ce5510b73a
Nazzareno Massari <nas@naszam.co>
2022-02-07 18:14:38 +0000

    fix case alignment and spacing

--- echidna/echidna.sh
+++ echidna/echidna.sh
@@ -4,17 +4,17 @@ set -e
 [[ "$1" ]] || { echo "Please specify the vest contract to fuzz (e.g. mintable, suckable, transferrable)"; exit 1; }
 
 case "$1" in
-            mintable)
-                VEST=DssVestMintableEchidnaTest
-                ;;
-            suckable)
-                VEST=DssVestSuckableEchidnaTest
-                ;;
+         mintable)
+             VEST=DssVestMintableEchidnaTest
+             ;;
+         suckable)
+             VEST=DssVestSuckableEchidnaTest
+             ;;
     transferrable)
-                VEST=DssVestTransferrableEchidnaTest
-                ;;
-    *)          echo "Incorrect vest contract: '$1' (mintable, suckable, transferrable)"; exit 1
-                ;;
+             VEST=DssVestTransferrableEchidnaTest
+             ;;
+    *)       echo "Incorrect vest contract: '$1' (mintable, suckable, transferrable)"; exit 1
+             ;;
 esac
 
 SOLC=~/.solc-select/artifacts/solc-0.6.12
