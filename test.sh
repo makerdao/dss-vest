392b23decc38af1e9b7f3d18e65d95f106994159
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-09-29 12:36:34 -0600

    remove dapp testing completely

--- test.sh
+++ test.sh
@@ -1,13 +1,42 @@
 #!/usr/bin/env bash
 set -e
 
-[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+[[ "$ETH_RPC_URL" && "$(cast chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-export DAPP_BUILD_OPTIMIZE=1
-export DAPP_BUILD_OPTIMIZE_RUNS=200
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
 
-if [[ -z "$1" ]]; then
-  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" -v
+    case "$KEY" in
+            match)           MATCH="$VALUE" ;;
+            match-test)      MATCH_TEST="$VALUE" ;;
+            match-contract)  MATCH_CONTRACT="$VALUE" ;;
+            block)           BLOCK="$VALUE" ;;
+            *)
+    esac
+done
+
+if [[ -z "$MATCH" && -z "$BLOCK" && -z "$MATCH_TEST" && -z "$MATCH_CONTRACT" ]]; then
+    forge test --fork-url "$ETH_RPC_URL" -vv --force --use 0.6.12
+elif [[ -z "$MATCH" && -z "$MATCH_TEST" && -z "$MATCH_CONTRACT" ]]; then
+    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" -vv --force --use 0.6.12
 else
-  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -vv
+    if [[ -n "$BLOCK" ]]; then
+        if [[ -n "$MATCH" ]]; then
+            forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force --use 0.6.12
+        elif [[ -n "$MATCH_TEST" ]]; then
+            forge test --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" --fork-block-number "$BLOCK" -vvv --force --use 0.6.12
+        else
+            forge test --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" --fork-block-number "$BLOCK" -vvv --force --use 0.6.12
+        fi
+    else
+        if [[ -n "$MATCH" ]]; then
+            forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force --use 0.6.12
+        elif [[ -n "$MATCH_TEST" ]]; then
+            forge test --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" -vvv --force --use 0.6.12
+        else
+            forge test --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" -vvv --force --use 0.6.12
+        fi
+    fi
 fi
