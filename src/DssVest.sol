a9cb04d963a23f65c674c43e0dddac8d8c5454d8
malteish <malteish@corpus.io>
2023-01-25 12:09:03 +0100

    bump solidity version to 0.8.17

--- src/DssVest.sol
+++ src/DssVest.sol
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity 0.6.12;
+pragma solidity 0.8.17;
 
 interface MintLike {
     function mint(address, uint256) external;
--- src/DssVest.t.sol
+++ src/DssVest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.6.12;
+pragma solidity 0.8.17;
 
 import "ds-test/test.sol";
 
