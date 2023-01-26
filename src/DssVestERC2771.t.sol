de6e387d8ff9a420c572396dac1b088a4d954fb5
malteish <malteish@corpus.io>
2023-01-26 09:52:23 +0100

    add first test with ERC2771

--- src/DssVestERC2771.t.sol
+++ src/DssVestERC2771.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.17;
 
-import "ds-test/test.sol";
+//import "ds-test/test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "@opengsn/contracts/src/forwarder/Forwarder.sol";
 
 
@@ -77,7 +78,8 @@ contract User {
     }
 }
 
-contract DssVestERC2771Test is DSTest {
+
+contract DssVestERC2771Test is Test {
     // --- Math ---
     uint256 constant WAD = 10**18;
     uint256 constant RAY = 10**27;
@@ -91,6 +93,19 @@ contract DssVestERC2771Test is DSTest {
     bytes32 domainSeparator;
     bytes32 requestType;
 
+    // addresses
+    // DO NOT USE IN PRODUCTION! Key was generated online for testing only.
+    uint256 public constant bossPrivateKey =
+        0x3c69254ad72222e3ddf37667b8173dd773bdbdfd93d4af1d192815ff0662de5f;
+    address public bossAddress = vm.addr(bossPrivateKey); // = 0x38d6703d37988C644D6d31551e9af6dcB762E618;
+
+    // DO NOT USE IN PRODUCTION! Key was generated online for testing only.
+    uint256 public constant usrPrivateKey =
+        0x8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f;
+    address public usrAddress = vm.addr(usrPrivateKey); // = 0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377;
+
+    address public constant relayer =
+        0xDFcEB49eD21aE199b33A76B726E2bea7A72127B0;
 
     // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
     bytes20 constant CHEAT_CODE =
@@ -169,18 +184,67 @@ contract DssVestERC2771Test is DSTest {
         // register request type with forwarder. Since the forwarder does not check the request type, we can use any string as function name.
         vm.recordLogs();
         forwarder.registerRequestType("someFunctionName", "some function parameters");
-        Vm.Log[] memory logs = vm.getRecordedLogs();
+        logs = vm.getRecordedLogs();
         // the next line extracts the request type from the event emitted by the forwarder
         requestType = logs[0].topics[1];
         console.log("requestType", vm.toString(requestType));
         require(forwarder.typeHashes(requestType), "Registering failed");
     }
 
-    function testCost() public {
-        new DssVestMintable(address(forwarder), address(gem));
-    }
+    function testInitERC2771() public {
+        // build request
+        bytes memory payload = abi.encodeWithSelector(
+            mVest.create.selector,
+            usrAddress, 
+            100 * days_vest, 
+            block.timestamp, 
+            100 days, 
+            0 days, 
+            bossAddress
+        );
 
-    function testInit() public {
+        IForwarder.ForwardRequest memory request = IForwarder.ForwardRequest({
+            from: usrAddress,
+            to: address(mVest),
+            value: 0,
+            gas: 1000000,
+            nonce: forwarder.getNonce(usrAddress),
+            data: payload,
+            validUntil: block.timestamp + 1 hours // like this, the signature will expire after 1 hour. So the platform hotwallet can take some time to execute the transaction.
+        });
+
+        bytes memory suffixData = "0";
+
+
+        // pack and hash request
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(
+                    forwarder._getEncoded(request, requestType, suffixData)
+                )
+            )
+        );
+
+        // sign request.        
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            usrPrivateKey,
+            digest
+        );
+        bytes memory signature = abi.encodePacked(r, s, v); // https://docs.openzeppelin.com/contracts/2.x/utilities
+
+        require(address(this) != usrAddress, "FWD: sender is the user");
+        vm.prank(address(this));
+        forwarder.execute(
+            request,
+            domainSeparator,
+            requestType,
+            suffixData,
+            signature
+        );
+
+        console.log("signing address: ", request.from);
         mVest.create(address(this), 100 * days_vest, block.timestamp, 100 days, 0 days, address(1));
         (address usr, uint48 bgn, uint48 clf, uint48 fin, address mgr,, uint128 tot, uint128 rxd) = mVest.awards(1);
         assertEq(usr, address(this));
