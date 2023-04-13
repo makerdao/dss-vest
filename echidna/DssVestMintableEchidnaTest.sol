a1679b8ab55a40a310748bb565056163f264e6ea
malteish <malteish@corpus.io>
2023-04-13 10:21:55 +0200

    update actions

--- echidna/DssVestMintableEchidnaTest.sol
+++ echidna/DssVestMintableEchidnaTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity 0.6.12;
+pragma solidity 0.8.17;
 
 import {DssVest, DssVestMintable} from "../src/DssVest.sol";
 import         {DSToken} from "./DSToken.sol";
--- echidna/DssVestSuckableEchidnaTest.sol
+++ echidna/DssVestSuckableEchidnaTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity 0.6.12;
+pragma solidity 0.8.17;
 
 import {DssVest, DssVestSuckable} from "../src/DssVest.sol";
 import        {ChainLog} from "./ChainLog.sol";
