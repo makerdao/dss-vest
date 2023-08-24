5d2bd3294e54fb15f52424cbf736c91ee5701a57
malteish <malteish@corpus.io>
2023-08-24 23:48:54 +0200

    deny deployer for logic contract

--- script/DeployMintableFactory.s.sol
+++ script/DeployMintableFactory.s.sol
@@ -14,13 +14,18 @@ contract DeployMintableCloneFactory is Script {
 
         console.log("Deploying from: ", deployerAddress );
 
-        address forwarder = vm.envAddress("FORWARDER_GOERLI");
+        address forwarder = vm.envAddress("FORWARDER_MAINNET");
         address gem = address(0xdeadbeef);
         uint256 cap = 42 * 10 ** 18;
         
         vm.startBroadcast(deployerPrivateKey);
         DssVestMintable mintable = new DssVestMintable(forwarder, gem, cap);
         console.log("DssVestMintable logic contract deployed at: ", address(mintable));
+
+        console.log("Removing ward from logic contract. Deployer is ward: ", mintable.wards(deployerAddress));
+        mintable.deny(deployerAddress);
+        console.log("Logic contract still has ward: ", mintable.wards(deployerAddress));
+
         
         DssVestMintableCloneFactory factory = new DssVestMintableCloneFactory(address(mintable));
         console.log("DssVestMintableCloneFactory deployed at: ", address(factory));
